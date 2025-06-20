fefe3271a311396bd5f7d202286f0fe6efb8de7f
Aman Rawat <42992769+rawataman101@users.noreply.github.com>
2025-06-20 22:55:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import './App.css';
 
-export default function App() {
+function App() {
   const [countries, setCountries] = useState([]);
+
   useEffect(() => {
     fetch("https://xcountries-backend.azurewebsites.net/all")
-      .then((res) => res.json())
+      .then((response) => response.json())
       .then((data) => setCountries(data))
-      .catch((err) => console.log("Error fetching API"));
-  });
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
   const containerStyle = {
     display: "flex",
     flexWrap: "wrap",
-    justifyContent: "space-evenly",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
   };
-  const cardStyle = {
-    width: "200px",
+
+  const countryStyle = {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    border: "1px solid black",
-    padding: "10px",
     margin: "10px",
+    padding: "10px",
+    border: "1px solid #ccc",
+    borderRadius: "10px",
+    width: "200px",
   };
-  const cardImgStyle = {
-    objectfit: "contain",
+
+  const flagStyle = {
     width: "100px",
     height: "100px",
   };
+
   return (
     <div style={containerStyle}>
       {countries.map((country) => (
-        <div style={cardStyle}>
-          <img
-            src={country.flag}
-            alt={`Flag of ${country.abbr}`}
-            style={cardImgStyle}
-          />
-          <h2 style={{ textAlign: "center" }}>{country.name}</h2>
+        <div key={country.cca3} style={countryStyle}>
+          <img src={country.flag} alt={country.name} style={flagStyle} />
+          <h2>{country.name}</h2>
         </div>
       ))}
     </div>
   );
 }
+
+export default App;
